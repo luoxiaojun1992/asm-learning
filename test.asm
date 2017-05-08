bb2fee95bcecae234dd5c4df0eaee0bf00171b18
罗晓俊 <luoxiaojun1992@sina.cn>
2017-05-08 14:49:32 +0800

    refactory

--- test.asm
+++ test.asm
@@ -8,6 +8,8 @@ section .data
     sys_write           :  equ   0x2000004
     sys_read            :  equ   0x2000003
     sys_exit            :  equ   0x2000001
+    stdin               :  equ   0
+    stdout              :  equ   1
   
 section .bss  
     in_char:            resw 4  
@@ -17,27 +19,27 @@ section .text
 _start:  
 l1:
     mov rax, sys_write         ; syscall需要用到的参数，表示write  
-    mov rdi, 1                 ; 表示stdout  
+    mov rdi, stdout           ; 表示stdout  
     mov rsi, query_string      ; syscall调用回到rsi来获取字符  
     mov rdx, query_string_len  ; 并到rdx获取长度  
     syscall  
   
     ; 读取字符  
     mov rax, sys_read          ; syscall需要用到的参数，表示read  
-    mov rdi, 0                 ; 表示stdin  
+    mov rdi, stdin             ; 表示stdin  
     mov rsi, in_char           ; 字符存储位置，在.bbs段  
     mov rdx, 2                 ; 从内核缓存获取两个字节,一个给字符一个给回车  
     syscall  
   
     ; 打印输入的值  
     mov rax, sys_write    
-    mov rdi, 1                
+    mov rdi, stdout                
     mov rsi, out_string  
     mov rdx, out_string_len  
     syscall  
   
     mov rax, sys_write  
-    mov rdi, 1            
+    mov rdi, stdout            
     mov rsi, in_char  
     mov rdx, 2                 ; 这里两个字节，因为第二个是回车   
     syscall  
@@ -46,5 +48,5 @@ l1:
 
     ; 退出syscall  
     mov rax, sys_exit          ; syscall需要用到的参数，表示退出syscall  
-    mov rdi, 0  
+    mov rdi, stdin  
     syscall
